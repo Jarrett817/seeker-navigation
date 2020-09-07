2af53f4d343af6cc9b1009d5d1a6dbd966ce47cf
Jarrett <1206905682@qq.com>
2020-09-07 15:08:57 +0800

    添加了ico图标

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const hashMap = xObject || [{
         url: 'https://www.bilibili.com'
     }
 ]
-
+//对输入的字符串格式化
 const simplifyUrl = (url) => {
     return url.replace('http://', '').replace('https://', '').
     replace('www.', '').replace(/\/.*/, '')
